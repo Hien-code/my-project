d1523f0bbfc56ede1d47b5116f3fc6d4b8654687
Hien-code <nguyenconghien.18092002@gmail.com>
2025-07-28 23:42:48 +0700

    Fix: remove invalid import from server/reply

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,4 @@
 //[GET] /products
-const { status } = require("server/reply");
 const Product = require("../../models/product.model");
 module.exports.index = async (req, res) => {
   const products = await Product.find({
