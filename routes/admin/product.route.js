09fc87bea64a2e50bb442255396a299f4b34cfaa
Hien-code <nguyenconghien.18092002@gmail.com>
2025-07-29 20:26:26 +0700

    6. Đẩy file tĩnh lên Cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 //Up load photo
 const multer = require("multer");
+
 //Updata name photo
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 router.get("/", controller.index);
 router.patch("/change-status/:status/:id", controller.changeStatus);
@@ -17,9 +18,11 @@ router.delete("/delete/:id", controller.deleteItem);
 router.patch("/restore/:id", controller.restore);
 router.delete("/delete-hard/:id", controller.hardDelete);
 router.get("/create", controller.create);
+
 router.post(
   "/create",
   upload.single("thumbnail"),
+  uploadCloud.upload,
   validate.createPost,
   controller.createPost
 );
