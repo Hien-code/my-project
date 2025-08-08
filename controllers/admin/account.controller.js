7d886fbea5b1ec890d1dc8dfd9efd756a1bb23c9
Hien-code <nguyenconghien.18092002@gmail.com>
2025-08-08 23:02:39 +0700

    Chỉnh sửa trang danh sách account

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,12 +1,22 @@
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 const systemConfig = require("../../config/system");
+const md5 = require("md5");
 
 //[GET] admin/accounts
 module.exports.index = async (req, res) => {
   const find = {
     delete: false,
   };
-  const records = await Account.find(find);
+  const records = await Account.find(find).select("-password -token");
+
+  for (const record of records) {
+    const role = await Role.findOne({
+      delete: false,
+      _id: record.role_id,
+    });
+    record.role = role;
+  }
   res.render("admin/pages/accounts/index", {
     pageTitle: "Trang accounts",
     records: records,
@@ -15,14 +25,31 @@ module.exports.index = async (req, res) => {
 
 //[GET] admin/accounts/create
 module.exports.create = async (req, res) => {
+  const roles = await Role.find({
+    delete: false,
+  });
+
   res.render("admin/pages/accounts/create", {
-    pageTitle: "Trang accounts",
+    pageTitle: "Trang tạo account",
+    roles: roles,
   });
 };
 
 //[POST] admin/accounts/create
 module.exports.createPost = async (req, res) => {
-  const record = new Account(req.body);
-  record.save();
+  const emailExist = await Account.findOne({
+    email: req.body.email,
+    delete: false,
+  });
+  console.log(emailExist);
+
+  if (emailExist) {
+    req.flash("error", `Email ${req.body.email} đã tồn tại!`);
+    res.redirect(req.get("referer"));
+  } else {
+    req.body.password = md5(req.body.password);
+    const record = new Account(req.body);
+    record.save();
+  }
   res.redirect(`${systemConfig.prefixAdmin}/accounts`);
 };
