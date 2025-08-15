b26788a8e57af1152cb7c847255ea1978577e7bc
Hien-code <nguyenconghien.18092002@gmail.com>
2025-08-15 10:31:49 +0700

    Trang show sản phẩm tìm kiếm

---
+++ controllers/client/search.controller.js
@@ -0,0 +1,22 @@
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
+
+//[GET] /search
+module.exports.index = async (req, res) => {
+  const keyword = req.query.keyword;
+  let newProducts = [];
+  if (keyword) {
+    const regex = new RegExp(keyword, "i");
+    const products = await Product.find({
+      title: regex,
+      delete: false,
+      status: "active",
+    });
+    newProducts = productsHelper.priceNewProducts(products);
+  }
+
+  res.render("client/pages/search/index", {
+    keyword: keyword,
+    products: newProducts,
+  });
+};
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,5 +1,6 @@
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
+const searchRoutes = require("./search.route");
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 
 //module.exports => require(import)
@@ -8,4 +9,5 @@ module.exports = (app) => {
   app.use(categoryMiddleware.category);
   app.use("/", homeRoutes);
   app.use("/products", productRoutes);
+  app.use("/search", searchRoutes);
 };
